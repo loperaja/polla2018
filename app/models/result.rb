077a9cd66e84fdbfc3d93813c3a7c70ce8dc22a1
Ricardo Lopera <loperaja@gmail.com>
2018-05-30 14:38:17 +0100

    added custom attributes for polymorphic

--- app/models/result.rb
+++ app/models/result.rb
@@ -2,10 +2,24 @@ class Result < ApplicationRecord
 
   belongs_to :resultable, polymorphic: true
   belongs_to :polla
+
+  MATCHES_PER_GROUP = 6
   
   def group
-    Match.find(self.match_id).group
-
+    case resultable_type
+    when "Match"
+      resultable.try(:group)
+    when "Round"
+      resultable.name.try(:first)
+    else
+      nil
+    end
   end
+
+  def position_teams_options
+    Team.where(group: group).map{|team| [team.name, team.id]}
+  end
+    
+
 
 end
