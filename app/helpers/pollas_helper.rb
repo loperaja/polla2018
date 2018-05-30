415c0860f3d4dc9c67ad18e5926735c64ebf77eb
Ricardo Lopera <loperaja@gmail.com>
2018-05-30 14:37:31 +0100

    added title helpers

--- app/helpers/pollas_helper.rb
+++ app/helpers/pollas_helper.rb
@@ -1,4 +1,30 @@
 module PollasHelper
 
+  def position_title(name)
+    pos = case name.last.to_i
+    when 1
+      "Primer"
+    when 2
+      "Segundo"
+    when 3
+     "Tercer"
+    when 4
+      "Cuarto"
+    else
+      nil
+    end
+
+    return "#{pos} lugar (#{name})"
+  end
+
+  def position_row_div(size, index)
+    per_group = 4
+    row = false
+    numcols = 12/size
+    if index % per_group == 0
+      row = (((index/per_group) % numcols) == 0)
+    end
+    return row
+  end
 
 end
