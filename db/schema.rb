fb867fb754ee7d1ed58aed4b266d68aefe725ab9
Ricardo Lopera <loperaja@gmail.com>
2018-05-30 14:41:56 +0100

    added new step row and index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_22_225301) do
+ActiveRecord::Schema.define(version: 2018_05_29_165438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,8 @@ ActiveRecord::Schema.define(version: 2018_05_22_225301) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "num_teams"
+    t.string "step"
+    t.index ["step"], name: "index_rounds_on_step"
   end
 
   create_table "teams", force: :cascade do |t|
