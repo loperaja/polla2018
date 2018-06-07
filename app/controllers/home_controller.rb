183726868075c1991c73d30545d6d330b8196d8a
Ricardo Lopera <loperaja@gmail.com>
2018-06-07 23:40:58 +0100

    changed chats display order

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
     @pollas = Polla.all
     @pollas_count = @pollas.count
     @user_pollas = current_user.pollas
-    @messages = Chat.order('created_at DESC').limit(50)
+    @messages = Chat.order('created_at ASC').limit(50)
     @message = Chat.new
   end
 end
