4e47bbca463115d1c7ffd74962ab5fc0289d2c52
Ricardo Lopera <loperaja@gmail.com>
2018-06-07 23:19:16 +0100

    chat channel js

---
+++ app/assets/javascripts/channels/chats.js
@@ -0,0 +1,36 @@
+//jQuery(document).on('turbolinks:load', function() {
+$(document).ready(function() {
+  var messages;
+  messages = $('#messages');
+  $(".chatcard").scrollTop($(".chatcard").prop("scrollHeight"));
+  if ($('#messages').length > 0) {
+    App.global_chat = App.cable.subscriptions.create({
+      channel: "ChatsChannel"
+    }, {
+      connected: function() {console.log('hello cable');},
+      disconnected: function() {},
+      received: function(data) {
+        messages.append(data['message']);
+        $(".chatcard").scrollTop($(".chatcard").prop("scrollHeight"));
+      },
+      send_message: function(message, author) {
+        return this.perform('send_message', {
+          message: message,
+          author: author
+        });
+      }
+    });
+    $('#new_chat').submit(function(e) {
+      var $this, textarea;
+      $this = $(this);
+      textarea = $this.find('#chat_message');
+      author = $this.find('#chat_author').val();
+      if ($.trim(textarea.val()).length > 1) {
+        App.global_chat.send_message(textarea.val(), author);
+        textarea.val('');
+       }
+       e.preventDefault();
+       return false;
+    });
+  }
+});
