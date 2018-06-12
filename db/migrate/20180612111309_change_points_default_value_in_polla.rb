4797c186390d0952281678dc950b526e1486f0ef
Ricardo Lopera <rlopera@williamhill.co.uk>
2018-06-12 13:33:22 +0100

    fixed typo in migration

--- db/migrate/20180612111309_change_points_default_value_in_polla.rb
+++ db/migrate/20180612111309_change_points_default_value_in_polla.rb
@@ -1,6 +1,6 @@
 class ChangePointsDefaultValueInPolla < ActiveRecord::Migration[5.2]
   def change
-    remove_column :pollas, :pints
+    remove_column :pollas, :points
     add_column :pollas, :points, :integer, :default => 0
   end
   
