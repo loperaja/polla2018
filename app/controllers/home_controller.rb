a7f618f58b3b743c0180807499af9b8e2ef5ed2a
Ricardo Lopera <rlopera@williamhill.co.uk>
2018-06-12 13:26:11 +0100

    olrdered polla by points and hiding real

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @pollas = Polla.all
+    @pollas = Polla.where(real: false).order('points DESC')
     @pollas_count = @pollas.count
     @user_pollas = current_user.pollas
     @messages = Chat.order('created_at ASC').limit(50)
