312e046a81ce362283dff8688ca011a16f3066aa
Ricardo Lopera <rlopera@williamhill.co.uk>
2018-06-15 05:03:54 +0100

    fixed chat

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
     @pollas = Polla.where(real: false).order('points DESC')
     @pollas_count = @pollas.count
     @user_pollas = current_user.pollas
-    @messages = Chat.order('created_at ASC').limit(50)
+    @messages = Chat.order('created_at DESC').limit(50).reverse
     @message = Chat.new
   end
 end
