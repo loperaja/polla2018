8288358ae4d6bebbcd8d9390788c88b5c627377c
Ricardo Lopera <rlopera@williamhill.co.uk>
2018-06-18 19:13:06 +0100

    added current message status

--- app/assets/javascripts/channels/chats.js
+++ app/assets/javascripts/channels/chats.js
@@ -1,6 +1,7 @@
 jQuery(document).on('turbolinks:load', function() {
 //$(document).ready(function() {
   var messages;
+  var timeout = null;
   messages = $('#messages');
   $(".chatcard").scrollTop($(".chatcard").prop("scrollHeight"));
   if ($('#messages').length > 0) {
@@ -10,13 +11,25 @@ jQuery(document).on('turbolinks:load', function() {
       connected: function() {console.log('hello cable');},
       disconnected: function() {},
       received: function(data) {
-        messages.append(data['message']);
-        $(".chatcard").scrollTop($(".chatcard").prop("scrollHeight"));
+       if(data['message'] != undefined) {
+          messages.append(data['message']);
+          $(".chatcard").scrollTop($(".chatcard").prop("scrollHeight"));
+        } else if(data['typing'] == true) {
+          if($("#chat_message").data('uid') != data['uid']) {
+            $("#chatNotify").fadeIn();
+            timeout = setTimeout(function() {$("#chatNotify").hide();}, 1500);
+          }
+        }
       },
       send_message: function(message, author) {
         return this.perform('send_message', {
           message: message,
           author: author
+        });
+      },
+      show_status: function(uid) {
+        return this.perform('show_status', {
+          uid: uid
         });
       }
     });
@@ -36,5 +49,11 @@ jQuery(document).on('turbolinks:load', function() {
        e.preventDefault();
        return false;
     });
+
+  $("#chat_message").on("keypress", function() {
+    var uid = $('#chat_message').data('uid');
+    var notif = $("#chatNotify");
+    App.global_chat.show_status(uid);
+  });
   }
 });
