2aef3303795a303475bbbf99d19756d34d88effb
Ricardo Lopera <loperaja@gmail.com>
2018-07-03 04:16:32 +0100

    fixed error preventing results being added

--- app/models/result.rb
+++ app/models/result.rb
@@ -3,9 +3,9 @@ class Result < ApplicationRecord
 
   belongs_to :resultable, polymorphic: true
   belongs_to :polla
-  belongs_to :match, -> { where(results: {resultable_type: 'Match'}) }, foreign_key: 'resultable_id'
-  belongs_to :round, -> { where(results: {resultable_type: 'Round'}) }, foreign_key: 'resultable_id'
-  belongs_to :team, foreign_key: 'result'
+  #belongs_to :match, -> { where(results: {resultable_type: 'Match'}) }, foreign_key: 'resultable_id'
+  #belongs_to :round, -> { where(results: {resultable_type: 'Round'}) }, foreign_key: 'resultable_id'
+  #belongs_to :team, foreign_key: 'result'
   
   
   validates_uniqueness_of :result, scope: [:resultable_id, :polla_id, :resultable_type]
